48dcc0ffaa88662b179c41c43be1bc5b926093cd
Surabhi0404 <surabhigurav96@gmail.com>
2019-11-19 12:50:06 -0500

    Added two tables BillingDetails and PaymentDetails

--- src/main/resources/Sprin2.sql
+++ src/main/resources/Sprin2.sql
@@ -49,3 +49,19 @@ payment_id int(25) PRIMARY KEY,
 payment_date DATETIME
 
 );
+
+create table Billing_details (
+user_id int primary key auto_increment,
+add_line1 varchar(25),
+add_line2 varchar(25),
+city varchar(25),
+pincode int,
+state varchar(25)
+);
+
+create table Payment_details(
+user_id INT primary key auto_increment,
+user_name varchar(25),
+card_no double NOT NULL,
+ccv int NOT NULL
+);
